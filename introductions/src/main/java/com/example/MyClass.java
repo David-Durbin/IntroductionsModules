6636bbce706f3427189f982809a576cda45ac7c4
David Durbin <ddurbin@e104-m-14.mchenry.edu>
2015-09-01 12:58:31 -0500

    Initial commit

---
+++ introductions/src/main/java/com/example/MyClass.java
@@ -0,0 +1,22 @@
+package com.example;
+
+public class MyClass
+{
+
+
+    public static void main(String[] args)
+    {
+
+        String myName = "David Durbin";
+        String major = "General IT Systems Management";
+        double gpa = 4.0;
+        char grade = 'A';
+        int started = 2004;
+
+        System.out.println("Hello, my name is " + myName + ".");
+        System.out.println("I am majoring in " + major + ".");
+        System.out.println("I currently have a GPA of " + gpa + ".");
+        System.out.println("I started at MCC in " + started + ".");
+
+    }
+}
